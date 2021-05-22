2d1131998846ba9bfba96cc4528bf480186df608
bashir ahammed <bahammed16@gmail.com>
2021-05-22 18:22:37 +0400

    sever  done

--- app.js
+++ app.js
@@ -5,19 +5,20 @@ import cors from 'cors'
 import configdb from './config/db.js'
 import router from './router/timeRouter.js'
 dotenv.config();
-const port =process.env.PORT || 5000
-const app =express()
+
+const app =express();
 app.use(express.json())
+
 app.use(cors())
 
 
-configdb()
+configdb();
 
 app.use('/api', router)
-app.get('/', (req, res)=>{
-    res.send('Hello world')
-})
 
-app.listen(port, ()=>{
-    console.log(`Sever run on port : ${port}`);
-})
+app.get('/', (req,res) => res.send('hello world'))
+
+const port =process.env.PORT || 5000
+
+
+app.listen(port, ()=>console.log(`Server will be run on port : ${port}`));
